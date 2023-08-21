3152cdc49c43acbcbc2b6bb1282b246613a6afed
f.salarian <f.salarian@mihaninsurance.com>
2023-08-21 10:24:08 +0330

    services page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "index.html", "contactUs.html"],
+  content: [
+    "./src/**/*.{html,js}",
+    "index.html",
+    "contactUs.html",
+    "galery.html",
+    "services.html",
+  ],
   extend: {
     fontFamily: {
       iranSans: ["iranSans", "sans-serif"],
